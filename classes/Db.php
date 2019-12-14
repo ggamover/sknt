5dcae8e2839c08c4684aff3cd57b5965c2d17114
Dmitry Evdokimov <info@gamover.ru>
2019-12-14 16:53:53 +0300

    Response parse url

--- classes/Db.php
+++ classes/Db.php
@@ -25,4 +25,5 @@ class Db
 	{
 		return $this->connection->query($query);
 	}
+
 }
--- classes/Request.php
+++ classes/Request.php
@@ -3,11 +3,32 @@
 
 namespace App\classes;
 
-
+/**
+ * Class Request
+ * @package App\classes
+ *
+ * @property string $method
+ * @property array $path
+ */
 class Request
 {
+	const REQUEST_METHOD_GET = 'GET';
+	const REQUEST_METHOD_PUT = 'PUT';
+
 	public function __construct()
 	{
+		$this->method = $_SERVER['REQUEST_METHOD'] ? $_SERVER['REQUEST_METHOD'] : 'GET';
+		$this->path = $this->parseUrl();
+	}
 
+	protected function parseUrl()
+	{
+		$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+		$absPath = $_SERVER['SCRIPT_FILENAME'];
+		$prefix = str_replace($_SERVER['DOCUMENT_ROOT'], '', $absPath);
+		$path = str_replace($prefix, '', $path);
+		$path = trim($path, '/');
+		$parts = explode('/', $path);
+		return array_filter($parts);
 	}
 }
